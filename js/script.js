5a772a85468a06ba11bafe78443cc72b0e8a2ea8
Cabot <victor.cabot@edu.devinci.fr>
2020-01-09 11:17:20 +0100

    UPDATE Connexion + Ajout patients

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function switch_font() {
+	document.getElementsByClassName("font-size")[0].style.fontWeight = "600";
+	document.getElementsByClassName("font-size1")[0].style.fontWeight = "300";
+};
+
+function switch_fonte() {
+	document.getElementsByClassName("font-size1")[0].style.fontWeight = "600";
+	document.getElementsByClassName("font-size")[0].style.fontWeight = "300";
+};
