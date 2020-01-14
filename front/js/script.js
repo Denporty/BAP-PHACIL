416d055a4a12ffaf46c9a4f48928e6994019ae9f
Cabot <victor.cabot@edu.devinci.fr>
2020-01-14 11:37:11 +0100

    ADD shop.html

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,9 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+function switch_font() {
+	document.getElementsByClassName("font-size")[0].style.fontWeight = "600";
+	document.getElementsByClassName("font-size1")[0].style.fontWeight = "300";
+};
+
+function switch_fonte() {
+	document.getElementsByClassName("font-size1")[0].style.fontWeight = "600";
+	document.getElementsByClassName("font-size")[0].style.fontWeight = "300";
+};
